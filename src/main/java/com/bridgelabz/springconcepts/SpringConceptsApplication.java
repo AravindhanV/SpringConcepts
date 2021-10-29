0a330a80278b1f13f2c138ca62291f13336f39db
Aravindhan V <aravindhan.v@perfios.com>
2021-10-29 19:57:24 +0530

    ADDED: SLF4j example

--- src/main/java/com/bridgelabz/springconcepts/SpringConceptsApplication.java
+++ src/main/java/com/bridgelabz/springconcepts/SpringConceptsApplication.java
@@ -1,12 +1,16 @@
 package com.bridgelabz.springconcepts;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringConceptsApplication {
+	public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
 
 	public static void main(String[] args) {
+		logger.debug("Welcome to Spring Boot Demo");
 		SpringApplication.run(SpringConceptsApplication.class, args);
 	}
 
