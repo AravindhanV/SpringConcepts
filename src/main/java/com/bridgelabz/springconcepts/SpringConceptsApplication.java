2f89c925f4574ab495a13e800c71083357d5e347
Aravindhan V <aravindhan.v@perfios.com>
2021-10-29 20:39:43 +0530

    ADDED: Employee and Department beans to show autowiring

--- src/main/java/com/bridgelabz/springconcepts/SpringConceptsApplication.java
+++ src/main/java/com/bridgelabz/springconcepts/SpringConceptsApplication.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.bridgelabz.springconcepts.component.EmployeeBean;
 
 @SpringBootApplication
 public class SpringConceptsApplication {
@@ -11,7 +14,11 @@ public class SpringConceptsApplication {
 
 	public static void main(String[] args) {
 		logger.debug("Welcome to Spring Boot Demo");
-		SpringApplication.run(SpringConceptsApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
+		EmployeeBean employeeBean = context.getBean(EmployeeBean.class);
+		employeeBean.setEid(104);
+		employeeBean.setEname("Spring Framework Guru");
+		employeeBean.showEmployeeDetails();
 	}
 
 }
